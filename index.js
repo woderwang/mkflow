bef1ebc31a59b6d4fa98ae9b060e971f8d9d53b1
woder.wang <woder.wang@gmail.com>
2020-07-30 09:38:23 +0800

    test

--- index.js
+++ index.js
@@ -8,9 +8,58 @@ let SimpleGitOptions = {
     maxConcurrentProcesses: 6,
 };
 const git = simpleGit(SimpleGitOptions);
+git.checkout(['-b', 'testbranch2']).then(e => { console.log(e) });
 // git.status().then(result => { console.log(result) });
 // git.add('./')
 //     .commit('commit test')
 //     .push();
 // git.merge({ from: 'master' }).then(e => { console.log(e) });
-git.merge(['master']).then(e=>{console.log(e)});
+// git.merge(['master']).then(e => { console.log(e) });
+/* feature */
+class feature {
+    constructor(props) {
+
+    }
+    start = () => {
+
+    }
+    finish = () => {
+
+    }
+};
+/* sit */
+class sit {
+    constructor(props) {
+
+    }
+    start = () => {
+
+    }
+    finish = () => {
+
+    }
+};
+
+// class uat {
+//     constructor(props) {
+
+//     }
+//     start = () => {
+
+//     }
+//     finish = () => {
+
+//     }
+// };
+
+// class hotfix {
+//     constructor(props) {
+
+//     }
+//     start = () => {
+
+//     }
+//     finish = () => {
+
+//     }
+// };
