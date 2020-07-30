2acbded97bed8c2ffe7c0ce1f39325f4a2220848
woder.wang <woder.wang@gmail.com>
2020-07-30 14:30:58 +0800

    test

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ class Feature {
             git.checkout(['-b', flowBranchName]).then(e => {
                 console.log('result', e);
             }, (err) => {
-                console.log(err.__proto__.git);
+                console.log(err.__proto__.checkoutSummary);
             })
         } catch (error) {
             // console.log(error);
