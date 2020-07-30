1d4302def526a2d46f92292fac8b10185bae245f
woder.wang <woder.wang@gmail.com>
2020-07-30 15:53:15 +0800

    test

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
-
-# mkflow
-custom git flow in own company
+"homepage": "https://github.com/woderwang/mkflow#readme",
+"dependencies": {
+  "simple-git": "^2.15.0",
+  "yargs": "^15.4.1"
+},
+"devDependencies": {
+  "colors": "^1.4.0"
+}
+}
