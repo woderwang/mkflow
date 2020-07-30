1a60b3bacc941f4342bc7b66d8e15cf6119bec21
woder.wang <woder.wang@gmail.com>
2020-07-30 15:55:42 +0800

    finish test

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "repository": {
-    "type": "git",
-    "url": "git+https://github.com/woderwang/mkflow.git"
-  },
+what fuck here;
