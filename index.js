a69b60cd2a48c53678d03fece9e9aec6c2a5fe5d
woder.wang <woder.wang@gmail.com>
2020-07-30 14:57:36 +0800

    test

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ class Feature {
                 if (err.stack) {
                     let stackContent = JSON.stringify(err.stack);
                     let atIndex = stackContent.indexOf(` at `);
-                    stackContent = stackContent.substring(1, atIndex - 4);
+                    stackContent = stackContent.substring(1, atIndex - 6);
                     // stackContent = stackContent.replace('/\n/\n', '');
                     console.log(stackContent);
                 }
