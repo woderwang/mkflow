209c8e627bcb8b113f5ca86908d0c8bf382fc76b
woder.wang <woder.wang@gmail.com>
2020-08-06 09:44:13 +0800

    test.js

--- bin/index.js
+++ bin/index.js
@@ -46,19 +46,7 @@ const flowConfig = {
         finishBranchs: [mkflowSetting.develop.branch, mkflowSetting.preStable.branch, mkflowSetting.stable.branch],
     }
 };
-/* demo block */
-/* ******* */
-// git.branch(['-l']).then(e => { console.log(e) });
-// git.checkout(['-b', 'testbranch2']).then(e => { console.log(e) });
-// git.status().then(result => { console.log(result) });
-// git.add('./')
-//     .commit('commit test')
-//     .push();
-// git.merge({ from: 'master' }).then(e => { console.log(e) });
-// git.merge(['master']).then(e => { console.log(e) });
-// git.branch().then(e => { console.log(e) });
-/* ******* */
-/* demo block end */
+
 
 
 class Flow {
---
+++ bin/test.js
@@ -0,0 +1,21 @@
+const simpleGit = require('simple-git');
+let SimpleGitOptions = {
+    baseDir: process.cwd(),
+    binary: 'git',
+    maxConcurrentProcesses: 6,
+};
+const git = simpleGit(SimpleGitOptions);
+/* demo block */
+/* ******* */
+git.status(['-uno']).then(e => { console.log(e) });
+// git.branch(['-l']).then(e => { console.log(e) });
+// git.checkout(['-b', 'testbranch2']).then(e => { console.log(e) });
+// git.status().then(result => { console.log(result) });
+// git.add('./')
+//     .commit('commit test')
+//     .push();
+// git.merge({ from: 'master' }).then(e => { console.log(e) });
+// git.merge(['master']).then(e => { console.log(e) });
+// git.branch().then(e => { console.log(e) });
+/* ******* */
+/* demo block end */
