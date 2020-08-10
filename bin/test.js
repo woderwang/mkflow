8e5c0e3c8db1590072470839575ec830891b8c5a
woder.wang <woder.wang@gmail.com>
2020-08-10 14:55:54 +0800

    test

--- bin/test.js
+++ bin/test.js
@@ -5,18 +5,19 @@ let SimpleGitOptions = {
     maxConcurrentProcesses: 6,
 };
 const git = simpleGit(SimpleGitOptions);
-git.push().then(e => {
-    console.log(e);
-}).catch(e => { console.log(e) });
-// (async function () {
-//     let resp;
-//     resp = await git.remote(['update']);
-//     console.log(resp);
-//     resp = await git.status();
-//     console.log(resp);
-// })();
+
+// git.push().then(e => {
+//     console.log(e);
+// }).catch(e => { console.log(e) });
+(async function () {
+    let resp;
+    resp = await git.remote(['update']);
+    console.log(resp);
+    resp = await git.status();
+    console.log(resp);
+})();
 /* ******* */
-console.log('test');
+
 // git.branch().then(e => { console.log(e) });
 // git.checkout(['-b', 'testbranch2']).then(e => { console.log(e) });
 // git.status().then(result => { console.log(result) });
