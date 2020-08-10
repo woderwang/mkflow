8d673c62908732f4867abe6d94b9b801b3c84401
woder.wang <woder.wang@gmail.com>
2020-08-10 15:02:01 +0800

    test

--- bin/index.js
+++ bin/index.js
@@ -126,8 +126,8 @@ class Flow {
                 console.log(colors.bgCyan(`remove remote branch ${flowBranchName} successful`));
             }
             // console.log(colors.yellow(`finish后目前不会提供自动push的操作，请手动执行push！`));
-        } catch (err = {}) {
-            if (err.git) {
+        } catch (err) {
+            if (err && err.git) {
                 const { merges, result } = err.git;
                 if (merges.length > 0) {
                     console.log(colors.red(result));
